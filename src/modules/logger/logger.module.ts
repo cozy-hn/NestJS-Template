8e45f8090b2e633d22035e2d4b45e83b4cfd45bb
cozy-hn <jhk2721@gmail.com>
2025-07-30 15:47:59 +0900

    chore: replace eslint and prettier with biome

--- src/modules/logger/logger.module.ts
+++ src/modules/logger/logger.module.ts
@@ -1,10 +1,10 @@
-import { Module, Global } from '@nestjs/common';
+import { Global, Module } from "@nestjs/common";
 
-import { LoggerService } from './logger.service';
+import { LoggerService } from "./logger.service";
 
 @Global()
 @Module({
-  providers: [LoggerService],
-  exports: [LoggerService],
+	providers: [LoggerService],
+	exports: [LoggerService],
 })
 export class LoggerModule {}
--- src/modules/redis/redis.module.ts
+++ src/modules/redis/redis.module.ts
@@ -1,28 +1,36 @@
-import { Module, Global } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
-import Redis from 'ioredis';
+import { Global, Module } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import Redis from "ioredis";
 
 @Global()
 @Module({
-  providers: [
-    {
-      provide: 'REDIS_CLIENT',
-      useFactory: (configService: ConfigService) => {
-        return new Redis({
-          password: configService.get<string>('REDIS_PASSWORD', 'password'),
-          db: 0,
-          sentinels: [
-            {
-              host: configService.get<string>('REDIS_SENTINEL_HOST', 'redis-sentinel'),
-              port: parseInt(configService.get<string>('REDIS_SENTINEL_PORT', '26379')),
-            },
-          ],
-          name: configService.get<string>('REDIS_SENTINEL_MASTER_NAME', 'mymaster'),
-        });
-      },
-      inject: [ConfigService],
-    },
-  ],
-  exports: ['REDIS_CLIENT'],
+	providers: [
+		{
+			provide: "REDIS_CLIENT",
+			useFactory: (configService: ConfigService) => {
+				return new Redis({
+					password: configService.get<string>("REDIS_PASSWORD", "password"),
+					db: 0,
+					sentinels: [
+						{
+							host: configService.get<string>(
+								"REDIS_SENTINEL_HOST",
+								"redis-sentinel",
+							),
+							port: parseInt(
+								configService.get<string>("REDIS_SENTINEL_PORT", "26379"),
+							),
+						},
+					],
+					name: configService.get<string>(
+						"REDIS_SENTINEL_MASTER_NAME",
+						"mymaster",
+					),
+				});
+			},
+			inject: [ConfigService],
+		},
+	],
+	exports: ["REDIS_CLIENT"],
 })
 export class RedisModule {}
